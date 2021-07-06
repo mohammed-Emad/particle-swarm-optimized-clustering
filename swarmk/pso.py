dc007cd89a03d4a23389e6ee9ff83d192daed8be
mohammed-Emad <mohammed.imad38@yahoo.com>
2021-07-06 22:18:42 +0200

    Update pso.py

--- swarmk/pso.py
+++ swarmk/pso.py
@@ -26,7 +26,7 @@ class ParticleSwarmOptimizedClustering:
         self.gbest_centroids = None
         self.gbest_sse = np.inf
         self._init_particles()
-    @numba.jit
+    @numba.njit()
     def _init_particles(self):
         for i in range(self.n_particles):
             particle = None
@@ -39,7 +39,7 @@ class ParticleSwarmOptimizedClustering:
                 self.gbest_score = particle.best_score
             self.particles.append(particle)
             self.gbest_sse = min(particle.best_sse, self.gbest_sse)
-    @numba.jit
+    @numba.njit()
     def run(self):
         print('Initial global best score', self.gbest_score)
         history = []
